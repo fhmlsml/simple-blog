d38f64a366c29281f37ddcc43bdc0d157659cd0d
fhmlsml <email.gemail10@gmail.com>
2024-05-17 13:50:39 +0700

    edit environment variable

--- csrf/settings.py
+++ csrf/settings.py
@@ -84,11 +84,11 @@ WSGI_APPLICATION = 'csrf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'postgresdb',
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 # DATABASES = {
